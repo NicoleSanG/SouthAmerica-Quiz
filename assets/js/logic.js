82cd3ebede1f82e7fc24a8725fa396f084be3de7
NicoleSG <nicolesantamariagranizo@gmail.com>
2023-12-31 22:03:05 +0000

    created logic.js file and declared variables

---
+++ assets/js/logic.js
@@ -0,0 +1,12 @@
+var start = document.querySelector('#start');
+var startScreen = document.querySelector('#start-screen');
+var questionsScreen = document.querySelector('#questions');
+var questionTitle = document.querySelector('#question-title');
+var choices = document.querySelector('#choices');
+var feedback = document.querySelector('#feedback');
+var timer = document.querySelector('#time');
+var endScreen = document.querySelector('#end-screen');
+var finalScore = document.querySelector('#final-score');
+var initials = document.querySelector('#initials');
+var submit = document.querySelector('#submit');
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,27 @@
 var questions = [
     {
-        question: 'Which South American country is known for the Galapagos Islands, a UNESCO World Heritage Site famous for its unique wildlife?',
+        title: 'Which South American country is known for the Galapagos Islands, a UNESCO World Heritage Site famous for its unique wildlife?',
         choices: ['Colombia', 'Ecuador', 'Peru', 'Brazil'],
         answer: 'Ecuador'
     },
 
     {
-        question: 'Which South American country is famous for Machu Picchu, an ancient Incan citadel located in the Andes Mountains?',
+        title: 'Which South American country is famous for Machu Picchu, an ancient Incan citadel located in the Andes Mountains?',
         choices: ['Peru', 'Ecuador', 'Venezuela', 'Chile'],
         answer: 'Peru'
     },
     {
-        question: 'The Amazon Rainforest, often referred to as the "lungs of the Earth," is primarily located in which South American country?',
+        title: 'The Amazon Rainforest, often referred to as the "lungs of the Earth," is primarily located in which South American country?',
         choices: ['Ecuador', 'Colombia', 'Venezuela', 'Brazil'],
         answer: 'Brazil'
     },
     {
-        question: "Which South American country is home to the iconic Angel Falls, the world's highest uninterrupted waterfall?",
+        title: "Which South American country is home to the iconic Angel Falls, the world's highest uninterrupted waterfall?",
         choices: ['Colombia', 'Argentina', 'Venezuela', 'Paraguay'],
         answer: 'Venezuela'
     },
     {
-        question: 'Which South American country is often referred to as the "City of Eternal Spring" due to its mild climate throughout the year?',
+        title: 'Which South American country is often referred to as the "City of Eternal Spring" due to its mild climate throughout the year?',
         choices: ['Colombia', 'Bolivia', 'Uruguay', 'Guyana'],
         answer: 'Colombia'
     }
